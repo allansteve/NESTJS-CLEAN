2df22df043c72e23203a020f107140f3f8538ee5
Allan Inácio <allaninacio@MacBook-Air-de-Allan.local>
2024-01-26 12:15:36 -0300

    Configuração inicial, Configuração da Autenticação com JWT RS256, Criação do Decorator Customizado.

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodyschema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodyschema = z.infer<typeof createAccountBodyschema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodyschema))
+  async handle(@Body() body: CreateAccountBodyschema) {
+    const { name, email, password } = body
+
+    const userAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userAlreadyExists) {
+      throw new ConflictException(
+        'User with same e-mail address already exists',
+      )
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+    })
+  }
+}
---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor() {}
+
+  @Post()
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
+
+    return 'ok'
+  }
+}
